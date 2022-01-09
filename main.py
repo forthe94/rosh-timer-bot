8dbd65ddd8ab4dc4f782add197cc758b4b147d5b
Alexander Tetenkov <forthe@ya.ru>
2022-01-09 17:50:03 +0300

    Add stop timer feature

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import os
 from aiotg import Bot, Chat
 
 bot = Bot(api_token=os.getenv('ROSH_TIMER_BOT_API_TOKEN'))
-
+timers_running = {}
 inline_keyboard_markup = {
     'type': 'InlineKeyboardMarkup',
     'inline_keyboard': [
         [{'type': 'InlineKeyboardButton',
           'text': 'Start timer',
-          'callback_data': 'start-timer'}]
+          'callback_data': 'start-timer'}],
+        [{'type': 'InlineKeyboardButton',
+          'text': 'Stop timer',
+          'callback_data': 'stop-timer'}]
     ]
 }
 phrases_delays = [
@@ -25,13 +28,25 @@ phrases_delays = [
 ]
 
 
+@bot.callback(r"stop-timer")
+async def stop_timer(chat, cq, match):
+    timers_running[chat.id] = False
+    await chat.send_text('Timer stopped', reply_markup=json.dumps(inline_keyboard_markup))
+
+
 @bot.callback(r"start-timer")
-async def echo(chat, cq, match):
+async def run_timer(chat, cq, match):
+    if timers_running.get(chat.id):
+        chat.send_text('Timer already running')
+        return
+    timers_running[chat.id] = True
     await chat.send_text('Timer started')
     for phraze, delay in phrases_delays:
-        await chat.send_text(phraze)
-        await asyncio.sleep(delay)
-
+        if timers_running[chat.id]:
+            await chat.send_text(phraze)
+            await asyncio.sleep(delay)
+        else:
+            return
     await chat.send_text('Rosh is up!', reply_markup=json.dumps(inline_keyboard_markup))
 
 
