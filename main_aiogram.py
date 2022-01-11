c6650b35b44ac211dd5aa109897a804c71d85d7f
Alexander Tetenkov <forthe@ya.ru>
2022-01-11 11:13:46 +0300

    Move to aiogram

--- main_aiogram.py
+++ main_aiogram.py
@@ -1,13 +1,20 @@
-import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 
 BOT_TOKEN = os.getenv('ROSH_TIMER_BOT_API_TOKEN')
 PORT = int(os.environ.get('PORT', 80))
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+start_timer_button = KeyboardButton('Start timer')
+
+reply_keyboard = ReplyKeyboardMarkup()
+reply_keyboard.add(start_timer_button)
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -18,6 +25,7 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 if __name__ == '__main__':
+    print(PORT)
     executor.start_webhook(
         dp,
         skip_updates=True,
